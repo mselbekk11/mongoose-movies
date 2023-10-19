1a76a7eb6bab2f8d01ce3b809478497cdc9c8c32
mselbekk11 <mselbekk11@gmail.com>
2023-10-19 16:52:11 -0700

    testing

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,37 +1,40 @@
-const Movie = require('../models/movie');
+const Movie = require('../models/movie')
 
 module.exports = {
   index,
   new: newMovie,
-  create
-};
+  create,
+}
 
 async function index(req, res) {
-  const movies = await Movie.find({});
-  res.render('movies/index', { movies });
+  const movies = await Movie.find({})
+  res.render('movies/index', { movies })
 }
 
 function newMovie(req, res) {
-  // We'll want to be able to render an  
+  // We'll want to be able to render an
   // errorMsg if the create action fails
-  res.render('movies/new', { errorMsg: '' });
+  res.render('movies/new', { errorMsg: '' })
 }
 
 async function create(req, res) {
   // convert nowShowing's checkbox of nothing or "on" to boolean
-  req.body.nowShowing = !!req.body.nowShowing;
+  req.body.nowShowing = !!req.body.nowShowing
   // remove any whitespace at start and end of cast
-  req.body.cast = req.body.cast.trim();
+  req.body.cast = req.body.cast.trim()
   // split cast into an array if it's not an empty string - using a regular expression as a separator
-  if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
+  if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/)
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key]
+  }
   try {
-    await Movie.create(req.body);
+    await Movie.create(req.body)
     // Always redirect after CUDing data
     // We'll refactor to redirect to the movies index after we implement it
-    res.redirect('/movies');  // Update this line
+    res.redirect('/movies') // Update this line
   } catch (err) {
     // Typically some sort of validation error
-    console.log(err);
-    res.render('movies/new', { errorMsg: err.message });
+    console.log(err)
+    res.render('movies/new', { errorMsg: err.message })
   }
 }
--- models/movie.js
+++ models/movie.js
@@ -1,16 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 // optional shortcut to the mongoose.Schema class
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
-const movieSchema = new Schema({
-  title: String,
-  releaseYear: Number,
-  mpaaRating: String,
+const movieSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  releaseYear: {
+    type: Number,
+    default: function () {
+      return new Date().getFullYear()
+    },
+    min: 1927,
+  },
+  mpaaRating: {
+    type: String,
+    enum: ['G', 'PG', 'PG-13', 'R'],
+  },
   cast: [String],
-  nowShowing: Boolean
-}, {
-  timestamps: true
-});
+  nowShowing: { type: Boolean, default: true },
+})
 
 // Compile the schema into a model and export it
-module.exports = mongoose.model('Movie', movieSchema);
+module.exports = mongoose.model('Movie', movieSchema)
